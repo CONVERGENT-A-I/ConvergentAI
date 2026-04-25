7eb8749f546f81709b88781bde89518ffef3c5dc
Hasan Kamal <shaheryarm46@gmail.com>
2026-04-26 00:53:43 +0500

    cta added again

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -39,7 +39,7 @@ export default function RootLayout({
         <BackendConnectionTest />
         {children}
         <Suspense fallback={null}>
-          {/*<FloatingCTA />*/}
+          <FloatingCTA />
         </Suspense>
         <Footer />
       </body>
